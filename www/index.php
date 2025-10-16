ab8f8ccf322a135ee831ab59fc37ed74a6749ce0
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-10-16 09:42:41 +0100

    Moved navbar

--- www/index.php
+++ www/index.php
@@ -18,34 +18,10 @@ if (isset($_GET["log-in"]) and $_GET["log-in"] == "yes") {
 </head>
 
 <body>
-    <div class="sidebar">
-        <div class="top">
-            <h2>Menu</h2>
-            <a href="/">Home</a>
-            <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']): ?>
-                <span class="nav-title">AIRPORT CREW</span>
-                <hr>
-                <a href="/check-in.php">Check-in</a>
-                <a href="/boarding.php">Boarding</a>
-                <a href="/display-pane.php">Flights Schedule</a>
-                <span class="nav-title">ADMINISTRATION</span>
-                <hr>
-                <a href="/admin/bookings.php">Booking Management</a>
-                <a href="/admin/passengers.php">Passenger Management</a>
-                <a href="/admin/users.php">User Management</a>
-                <a href="/admin/airports.php">Airport Management</a>
-                <a href="/admin/flights.php">Flight Management</a>
-                <a href="/admin/aircrafts.php">Aircraft Management</a>
-            <?php endif; ?>
-        </div>
-        <div>
-            <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']): ?>
-                <a href="/admin/?log-in=no">Log out</a>
-            <?php else: ?>
-                <a href="/?log-in=yes">Log in</a>
-            <?php endif; ?>
-        </div>
-    </div>
+
+    <?php
+        include ("internal/sidebar.php");
+    ?>
 
     <div class="content">
         <center>
---
+++ www/internal/sidebar.php
@@ -0,0 +1,28 @@
+<div class="sidebar">
+    <div class="top">
+        <h2><img style="width:100%" src="/static/images/logo.png"></h2>
+        <a href="/">Home</a>
+        <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']): ?>
+            <span class="nav-title">AIRPORT CREW</span>
+            <hr>
+            <a href="/check-in.php">Check-in</a>
+            <a href="/boarding.php">Boarding</a>
+            <a href="/display-pane.php">Flights Schedule</a>
+            <span class="nav-title">ADMINISTRATION</span>
+            <hr>
+            <a href="/admin/bookings.php">Booking Management</a>
+            <a href="/admin/passengers.php">Passenger Management</a>
+            <a href="/admin/users.php">User Management</a>
+            <a href="/admin/airports.php">Airport Management</a>
+            <a href="/admin/flights.php">Flight Management</a>
+            <a href="/admin/aircrafts.php">Aircraft Management</a>
+        <?php endif; ?>
+    </div>
+    <div>
+        <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']): ?>
+            <a href="/admin/?log-in=no">Log out</a>
+        <?php else: ?>
+            <a href="/?log-in=yes">Log in</a>
+        <?php endif; ?>
+    </div>
+</div>
