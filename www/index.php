513a2901ca5a1494026273f957866274adb57d9f
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-10-16 09:22:52 +0100

    Navbar initial setup

--- www/index.php
+++ www/index.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (isset($_GET["log-in"]) and $_GET["log-in"] == "yes") {
+    $_SESSION["loggedin"] = "yes";
+} else if (isset($_GET["log-in"]) and $_GET["log-in"] == "no") {
+    unset($_SESSION["loggedin"]);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,8 +18,42 @@
 </head>
 
 <body>
-    <h1>Domestic Airlines Management System</h1>
-    <img src="static/images/atr72.png">
+    <div class="sidebar">
+        <div class="top">
+            <h2>Menu</h2>
+            <a href="/">Home</a>
+            <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']): ?>
+                <span class="nav-title">AIRPORT CREW</span>
+                <hr>
+                <a href="/check-in.php">Check-in</a>
+                <a href="/boarding.php">Boarding</a>
+                <a href="/display-pane.php">Flights Schedule</a>
+                <span class="nav-title">ADMINISTRATION</span>
+                <hr>
+                <a href="/admin/bookings.php">Booking Management</a>
+                <a href="/admin/passengers.php">Passenger Management</a>
+                <a href="/admin/users.php">User Management</a>
+                <a href="/admin/airports.php">Airport Management</a>
+                <a href="/admin/flights.php">Flight Management</a>
+                <a href="/admin/aircrafts.php">Aircraft Management</a>
+            <?php endif; ?>
+        </div>
+        <div>
+            <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']): ?>
+                <a href="/admin/?log-in=no">Log out</a>
+            <?php else: ?>
+                <a href="/?log-in=yes">Log in</a>
+            <?php endif; ?>
+        </div>
+    </div>
+
+    <div class="content">
+        <center>
+            <h1>Domestic Airlines Management System</h1>
+            <img src="static/images/atr72.png">
+        </center>
+    </div>
+
 </body>
 
 </html>
