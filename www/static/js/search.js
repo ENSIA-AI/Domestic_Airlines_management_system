103ee6f8bb7b1bfb402333aeec1e55965700e8bf
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-10-23 14:43:35 +0100

    Backend airports

--- www/static/js/search.js
+++ www/static/js/search.js
@@ -17,3 +17,22 @@ function search() {
         }
     }
 }
+
+function postRedirect(url, params) {
+  const form = document.createElement('form');
+  form.method = 'POST';
+  form.action = url;
+
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      const input = document.createElement('input');
+      input.type = 'hidden';
+      input.name = key;
+      input.value = params[key];
+      form.appendChild(input);
+    }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+}
