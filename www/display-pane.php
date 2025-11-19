75eba289dfd5facdcfb6c90cf437bf0284e098a9
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-11-19 17:30:32 +0100

    stuff

--- www/display-pane.php
+++ www/display-pane.php
@@ -5,6 +5,7 @@ if (isset($_GET["log-in"]) and $_GET["log-in"] == "yes") {
 } else if (isset($_GET["log-in"]) and $_GET["log-in"] == "no") {
     unset($_SESSION["loggedin"]);
 }
+include "./internal/db_config.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,6 +21,12 @@ if (isset($_GET["log-in"]) and $_GET["log-in"] == "yes") {
     <div id="container">
         <?php if (isset($_GET["type"])): ?>
             <div class="logo-grid">
+                <?php
+                $sql = "SELECT * FROM AIRPORTS WHERE IATA_CODE='" . $_GET["airport"] . "'";
+                $result_airports = $conn->query($sql);
+                $airport = $result_airports->fetch_assoc();
+                ?>
+
                 <?php if (isset($_GET["type"]) and $_GET["type"] == "Departures"): ?>
                     <div>
                         <img class="logo" src="/static/images/dep.png">
@@ -40,12 +47,7 @@ if (isset($_GET["log-in"]) and $_GET["log-in"] == "yes") {
             </div>
             <p class="welcome-msg marquee">
                 <span><?php
-                $airport = $_GET["airport"];
-
-                if ($airport == "BJA")
-                    echo "WELCOME TO BEJAIA AIRPORT - ABANE RAMDANE [BJA]";
-                if ($airport == "ALG")
-                    echo "WELCOME TO ALGIERS AIRPORT - HOUARI BOUMEDIENE [ALG]";
+                echo "WELCOME TO " . $airport["WILAYA"] . " AIRPORT - " . $airport["DISPLAY_NAME"] . " [" . $airport["IATA_CODE"] . "]";
                 ?></span>
             </p>
             <div class="pane">
@@ -58,167 +60,28 @@ if (isset($_GET["log-in"]) and $_GET["log-in"] == "yes") {
                         <th>Gate</th>
                         <th>Remarks</th>
                     </tr>
-                    <tr>
-                        <td>06:00</td>
-                        <td>Algiers</td>
-                        <td>ALG</td>
-                        <td>AH6001</td>
-                        <td>A1</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>07:15</td>
-                        <td>Oran</td>
-                        <td>ORN</td>
-                        <td>AH6023</td>
-                        <td>B2</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>08:30</td>
-                        <td>Constantine</td>
-                        <td>CZL</td>
-                        <td>AH6045</td>
-                        <td>A2</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>09:00</td>
-                        <td>Setif</td>
-                        <td>QSF</td>
-                        <td>AH6067</td>
-                        <td>B1</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>09:45</td>
-                        <td>Annaba</td>
-                        <td>AAE</td>
-                        <td>AH6089</td>
-                        <td>A3</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>10:30</td>
-                        <td>Algiers</td>
-                        <td>ALG</td>
-                        <td>AH6012</td>
-                        <td>A1</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>11:15</td>
-                        <td>Batna</td>
-                        <td>BLJ</td>
-                        <td>AH6101</td>
-                        <td>B3</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>12:00</td>
-                        <td>Oran</td>
-                        <td>ORN</td>
-                        <td>AH6024</td>
-                        <td>B2</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>12:45</td>
-                        <td>Algiers</td>
-                        <td>ALG</td>
-                        <td>AH6002</td>
-                        <td>A1</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>13:30</td>
-                        <td>Constantine</td>
-                        <td>CZL</td>
-                        <td>AH6046</td>
-                        <td>A2</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>14:15</td>
-                        <td>Setif</td>
-                        <td>QSF</td>
-                        <td>AH6068</td>
-                        <td>B1</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>15:00</td>
-                        <td>Annaba</td>
-                        <td>AAE</td>
-                        <td>AH6090</td>
-                        <td>A3</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>15:45</td>
-                        <td>Algiers</td>
-                        <td>ALG</td>
-                        <td>AH6013</td>
-                        <td>A1</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>16:30</td>
-                        <td>Batna</td>
-                        <td>BLJ</td>
-                        <td>AH6102</td>
-                        <td>B3</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>17:15</td>
-                        <td>Oran</td>
-                        <td>ORN</td>
-                        <td>AH6025</td>
-                        <td>B2</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>18:00</td>
-                        <td>Algiers</td>
-                        <td>ALG</td>
-                        <td>AH6003</td>
-                        <td>A1</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>18:45</td>
-                        <td>Constantine</td>
-                        <td>CZL</td>
-                        <td>AH6047</td>
-                        <td>A2</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>19:30</td>
-                        <td>Setif</td>
-                        <td>QSF</td>
-                        <td>AH6069</td>
-                        <td>B1</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>20:15</td>
-                        <td>Annaba</td>
-                        <td>AAE</td>
-                        <td>AH6091</td>
-                        <td>A3</td>
-                        <td>On Time</td>
-                    </tr>
-                    <tr>
-                        <td>21:00</td>
-                        <td>Algiers</td>
-                        <td>ALG</td>
-                        <td>AH6014</td>
-                        <td>A1</td>
-                        <td>On Time</td>
-                    </tr>
+                    <?php if ($_GET["type"] == 'Departures'): ?>
 
+                        <?php
+                    $sql = "SELECT FLIGHT_NUMBER, DEPARTURE_TIME, AIRPORTS.WILAYA AS ARR_WILAYA, ARR_AIRPORT, DEP_GATE, STATUS FROM FLIGHTS LEFT JOIN AIRPORTS ON FLIGHTS.ARR_AIRPORT = AIRPORTS.IATA_CODE WHERE DEP_AIRPORT = '" . $_GET["airport"] . "' AND (DEPARTURE_TIME >= NOW() OR REAL_DEPARTURE_TIME >= NOW()) ORDER BY DEPARTURE_TIME LIMIT 30";
+                    $result_flights = $conn->query($sql);
+                    while ($row = $result_flights->fetch_assoc()):
+                        ?>
+                        <tr>
+                            <td><?= (new DateTime($row["DEPARTURE_TIME"]))->format('H:i') ?></td>
+                            <td><?= $row["ARR_WILAYA"] ?></td>
+                            <td><?= $row["ARR_AIRPORT"] ?></td>
+                            <td><?= $row["FLIGHT_NUMBER"] ?></td>
+                            <td><?= $row["DEP_GATE"] ?></td>
+                            <td><?= str_replace('_', ' ', $row["STATUS"]) ?></td>
+                        </tr>
+                    <?php endwhile; ?>
+
+                    <?php else: ?>
+
+                        Planes never arrive to this airport, sorry.
+
+                    <?php endif; ?>
                 </table>
             </div>
         <?php else: ?>
@@ -232,8 +95,13 @@ if (isset($_GET["log-in"]) and $_GET["log-in"] == "yes") {
                     <form>
                         <label>Select Airport:</label>
                         <select name="airport" aria-label="Airport">
-                            <option value="ALG">Algiers</option>
-                            <option value="BJA">Bejaia</option>
+                            <?php
+                            $sql = "SELECT IATA_CODE, WILAYA FROM AIRPORTS";
+                            $result_airports = $conn->query($sql);
+                            while ($row = $result_airports->fetch_assoc()):
+                                ?>
+                                <option value="<?= $row["IATA_CODE"] ?>"><?= $row["WILAYA"] ?></option>
+                            <?php endwhile; ?>
                         </select>
                         <br>
                         <input type="submit" name="type" value="Departures">
@@ -274,9 +142,9 @@ if (isset($_GET["log-in"]) and $_GET["log-in"] == "yes") {
         setInterval(() => {
             clockSpan.innerText = getCurrentTimeHHMMSS();
         }, 100);
-        getWeather("<?= ($airport == "BJA" ? "béjaïa" : "algiers") ?>");
+        getWeather("<?= $airport["WILAYA"] ?>");
         setInterval(() => {
-            getWeather("<?= ($airport == "BJA" ? "béjaïa" : "algiers") ?>");
+            getWeather("<?= $airport["WILAYA"] ?>");
         }, 2000000);
     </script>
 </body>
