4478d99b0fbd933de4561922be5084a2965c6476
mohamed-anis-hadj-ahmed <mohamed-anis.hadjahmed@ensia.edu.dz>
2025-11-19 10:55:42 +0100

    Modified Aircrafts

--- www/admin/aircrafts.php
+++ www/admin/aircrafts.php
@@ -25,93 +25,92 @@ if (isset($_GET["log-in"]) and $_GET["log-in"] == "yes") {
     include("../internal/sidebar.php");
     ?>
     <main class="content">
-        <div class="dams-head">
+        <div class="head_aircrafts">
             <h1 class="title">Aircrafts Management</h1>
             <button class="btn add-btn">
-                <i class="fa fa-plus"></i>
+                <i class="fa fa-plus"></i> Add New
             </button>
         </div>
 
         <div class="manage">
-            <div class="search-container">
+            <div class="search-part">
                 <h2 class="recent">Aircafts Table</h2>
                 <div class="search-bar"><input type="text" class="search" id="search-bar" placeholder="Search">
                     <button class="search-btn"><i class="fa fa-search"></i></button>
                 </div>
             </div>
-            <div class="table-container">
-                <table class="dams-table" id="search-table">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Model</th>
-                            <th>Registration Number</th>
-                            <th>Service Entry Date</th>
-                            <th>Capcity</th>
-                            <th>Status</th>
-                            <th>Options</th>
-                        </tr>
-                    </thead>
-                    <tbody class="aircraftsTbBody">
-                        <tr>
-                            <td>XJ9HE4</td>
-                            <td>Test num1</td>
-                            <td>AH1432</td>
-                            <td>16 Oct 2024</td>
-                            <td>12A</td>
-                            <td><span class="status confirmed">Confirmed</span></td>
-                            <td>
-                                <div class="options">
-                                    <button class="option"><i class="fa fa-edit"></i></button>
-                                    <button class="option"><i class="fa fa-trash"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>XJ9HE4</td>
-                            <td>Test num2</td>
-                            <td>AH0453</td>
-                            <td>16 Oct 2024</td>
-                            <td>12A</td>
-                            <td><span class="status maintenance">Pending</span></td>
-                            <td>
-                                <div class="options">
-                                    <button class="option"><i class="fa fa-edit"></i></button>
-                                    <button class="option"><i class="fa fa-trash"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>XJ9HE4</td>
-                            <td>Test num3</td>
-                            <td>AH0633</td>
-                            <td>16 Oct 2024</td>
-                            <td>12A</td>
-                            <td><span class="status out">Pending</span></td>
-                            <td>
-                                <div class="options">
-                                    <button class="option"><i class="fa fa-edit"></i></button>
-                                    <button class="option"><i class="fa fa-trash"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>XJ9HE4</td>
-                            <td>Test num4</td>
-                            <td>AH0443</td>
-                            <td>16 Oct 2024</td>
-                            <td>12A</td>
-                            <td><span class="status retired">Cancelled</span></td>
-                            <td>
-                                <div class="options">
-                                    <button class="option"><i class="fa fa-edit"></i></button>
-                                    <button class="option"><i class="fa fa-trash"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+
+            <table class="aircafts-table" id="search-table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Model</th>
+                        <th>Registration Number</th>
+                        <th>Service Entry Date</th>
+                        <th>Capcity</th>
+                        <th>Status</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+                <tbody class="aircraftsTbBody">
+                    <tr>
+                        <td>XJ9HE4</td>
+                        <td>Test num1</td>
+                        <td>AH1432</td>
+                        <td>16 Oct 2024</td>
+                        <td>12A</td>
+                        <td><span class="status confirmed">Confirmed</span></td>
+                        <td>
+                            <div class="options">
+                                <button class="option"><i class="fa fa-edit"></i></button>
+                                <button class="option"><i class="fa fa-trash"></i></button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XJ9HE4</td>
+                        <td>Test num2</td>
+                        <td>AH0453</td>
+                        <td>16 Oct 2024</td>
+                        <td>12A</td>
+                        <td><span class="status maintenance">Pending</span></td>
+                        <td>
+                            <div class="options">
+                                <button class="option"><i class="fa fa-edit"></i></button>
+                                <button class="option"><i class="fa fa-trash"></i></button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XJ9HE4</td>
+                        <td>Test num3</td>
+                        <td>AH0633</td>
+                        <td>16 Oct 2024</td>
+                        <td>12A</td>
+                        <td><span class="status out">Pending</span></td>
+                        <td>
+                            <div class="options">
+                                <button class="option"><i class="fa fa-edit"></i></button>
+                                <button class="option"><i class="fa fa-trash"></i></button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XJ9HE4</td>
+                        <td>Test num4</td>
+                        <td>AH0443</td>
+                        <td>16 Oct 2024</td>
+                        <td>12A</td>
+                        <td><span class="status retired">Cancelled</span></td>
+                        <td>
+                            <div class="options">
+                                <button class="option"><i class="fa fa-edit"></i></button>
+                                <button class="option"><i class="fa fa-trash"></i></button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </main>
 </body>
